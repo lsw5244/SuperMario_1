381467255fe451af14d2826b6834deb1efc1e739
이성우 <lsw5244@naver.com>
2021-12-07 20:41:10 +0900

    feat : Config.h를 나눌 h파일들 추가 및 EnumClass나눔
    EnumClassCollection : EnumClass를 모을 헤더파일
    GlobalVariableCollection : 전역 변수들을 모을 헤더파일
    HeaderCollection : 포함 할 기본적인 헤더파일들을 모을 헤더파일
    MacroCollection : #define으로 만든 메크로들을 저장 할 헤더파일
    StructCollection : 구조체들을 저장 할 헤더파일

--- Config.h
+++ Config.h
@@ -52,20 +52,6 @@ extern POINT g_ptMouse;
 
 #define TILE_SIZE 16		// 가로 세로 16픽셀
 
-enum class MoveDirection
-{
-	Left, Right, End
-};
-
-enum class BlockType
-{
-	Brick, CanDestroyBrick, ItemBlock, CoinBlock, StarBlock, HiddenCoinBlcok, HiddenStarBlock,// 밟을 수 있는 블럭
-	Coin,																																// 닿으면 점수 오르는 블럭
-	Background, FlagEnd,																										// 구름, 산 등 배경 블럭
-	Flag,  FlagPole, FlagTop,																									//깃발(닿으면 게임 클리어)
-	LateRenderBlock																												// 플레이어 랜더 후 랜더되는 블럭
-};
-
 struct SampleTile
 {
 	RECT rc;
