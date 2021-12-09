90c4e96746ead8fbbbe6efb271b5261815457eca
Ojingeo <33677312+lsw5244@users.noreply.github.com>
2021-12-09 15:59:04 +0900

    Chore : Change from get message to peek message (#4)
    * Chore : 메시지를 받는 함수를 GetMessage -> PeekMessage로 변경
    
    * Refactor : 타이머 객체 삭제, 사용하지 않는 변수 제거

--- MainGame.cpp
+++ MainGame.cpp
@@ -20,11 +20,8 @@ HRESULT MainGame::Init()
 
 	//srand((unsigned int) time(nullptr));
 
-	// 타이머 셋팅
-	hTimer = (HANDLE)SetTimer(g_hWnd, 0, 10, NULL);
-
-	clickedMousePosX = 0; 
-	clickedMousePosY = 0; 
+	//clickedMousePosX = 0; 
+	//clickedMousePosY = 0; 
 
 	// 백버퍼
 	backBuffer = new Image;
@@ -67,9 +64,6 @@ void MainGame::Release()
 	SceneManager::GetInstance()->Release();
 
 	GameDataContainer::GetInstance()->Release();
-
-	// 타이머 객체 삭제
-	KillTimer(g_hWnd, 0);
 }
 
 
@@ -78,24 +72,10 @@ LRESULT MainGame::MainProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lPara
 	switch (iMessage)
 	{
 	case WM_KEYDOWN:
-		switch (wParam)
-		{
-		case VK_SPACE:
-			break;
-		case VK_UP:
-			break;
-		case VK_DOWN:
-			break;
-		case VK_LEFT:
-			break;
-		case VK_RIGHT:
-			break;
-		}
 		break;
-
 	case WM_LBUTTONDOWN:
-		clickedMousePosX = LOWORD(lParam);
-		clickedMousePosY = HIWORD(lParam);
+		//clickedMousePosX = LOWORD(lParam);
+		//clickedMousePosY = HIWORD(lParam);
 		break;
 	case WM_MOUSEMOVE:
 		g_ptMouse.x = LOWORD(lParam);
