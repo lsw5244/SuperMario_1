3961a0ff225293a1aae927cc13f01e42abb1fc7b
이성우 <lsw5244@naver.com>
2021-12-17 19:31:39 +0900

    feat : AmmoManager.h정의

--- AmmoManger.cpp
+++ AmmoManger.cpp
@@ -1,1 +1,18 @@
 #include "AmmoManger.h"
+
+HRESULT AmmoManger::Init()
+{
+	return S_OK;
+}
+
+void AmmoManger::Update()
+{
+}
+
+void AmmoManger::Render(HDC hdc)
+{
+}
+
+void AmmoManger::Release()
+{
+}
--- AmmoManger.h
+++ AmmoManger.h
@@ -1,16 +1,18 @@
 #pragma once
 #include "GameEntity.h"
+#include <vector>
 
 class FireAmmo;
 class AmmoManger : public GameEntity
 {
 private:
+	std::vector<FireAmmo*> ammos;
 
 public:
 	virtual HRESULT Init() override;
 	virtual void Update() override;
 	virtual void Render(HDC hdc) override;
 	virtual void Release() override;
-	virtual ~AmmoManger();
+	virtual ~AmmoManger() = default;
 };
 
