03f503ab17eb97efadfc52c7dfea0c3c936f2324
이성우 <lsw5244@naver.com>
2021-12-21 16:42:34 +0900

    feat : 타이틀 씬 기본 요소 추가

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -1,1 +1,18 @@
 #include "TitleScene.h"
+
+HRESULT TitleScene::Init()
+{
+	return E_NOTIMPL;
+}
+
+void TitleScene::Update()
+{
+}
+
+void TitleScene::Render(HDC hdc)
+{
+}
+
+void TitleScene::Release()
+{
+}
--- TitleScene.h
+++ TitleScene.h
@@ -1,5 +1,14 @@
 #pragma once
-class TitleScene
+#include "GameEntity.h"
+class TitleScene : public GameEntity
 {
+private:
+
+public:
+	virtual HRESULT Init() override;
+	virtual void Update() override;
+	virtual void Render(HDC hdc) override;
+	virtual void Release() override;
+	virtual ~TitleScene() = default;
 };
 
